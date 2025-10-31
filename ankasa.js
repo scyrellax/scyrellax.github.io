c3956a4102fb07df9547ee967ea6a7d3bf5879c9
cyjen <130879339+scyrellax@users.noreply.github.com>
2025-10-31 21:03:54 +0800

    Create ankasa.js

---
+++ ankasa.js
@@ -0,0 +1,34 @@
+window.addEventListener("load", () => {
+  const audio = document.getElementById("tune");
+  const playIcon = document.querySelector(".playy");
+  const pauseIcon = document.querySelector(".pausee");
+
+  // delay for loading
+  setTimeout(() => {
+    // force icons
+    playIcon.style.display = "none";
+    pauseIcon.style.display = "block";
+
+    audio.play().catch(() => {
+      playIcon.style.display = "block";
+      pauseIcon.style.display = "none";
+    });
+  }, 300);
+});
+
+function songstart() {
+  const audio = document.getElementById("tune");
+  const playIcon = document.querySelector(".playy");
+  const pauseIcon = document.querySelector(".pausee");
+
+  if (audio.paused) {
+    audio.muted = false; // unmute when user interacts
+    audio.play();
+    playIcon.style.display = "none";
+    pauseIcon.style.display = "block";
+  } else {
+    audio.pause();
+    playIcon.style.display = "block";
+    pauseIcon.style.display = "none";
+  }
+}
